006cbbbec9c400d766032d0999980b1d109a6fd7
GanstaZ <ganstaz@hotmail.com>
2019-01-16 04:30:15 +0200

    Update code and fix an issue

--- README.md
+++ README.md
@@ -12,30 +12,30 @@ and can be installed using [Composer](http://getcomposer.org/). Alternatively yo
 Create an instance of the `Solaris\MoonPhase` class, supplying a UNIX timestamp for when you want to determine the moon phase (if you don't then the current time will be used). You can then use the following class functions to access the properties of the object:
 
  - `phase()`: the terminator phase angle as a fraction of a full circle (i.e., 0 to 1). Both 0 and 1 correspond to a New Moon, and 0.5 corresponds to a Full Moon.
- - `illumination()`: the illuminated fraction of the Moon (0 = New, 1 = Full).
- - `age()`: the age of the Moon, in days.
- - `distance()`: the distance of the Moon from the centre of the Earth (kilometres).
- - `diameter()`: the angular diameter subtended by the Moon as seen by an observer at the centre of the Earth (degrees).
- - `sundistance()`: the distance to the Sun (kilometres).
- - `sundiameter()`: the angular diameter subtended by the Sun as seen by an observer at the centre of the Earth (degrees).
- - `new_moon()`: the time of the last New Moon (UNIX timestamp).
- - `next_new_moon()`: the time of the next New Moon (UNIX timestamp).
- - `full_moon()`: the time of the Full Moon in the current lunar cycle (UNIX timestamp).
- - `next_full_moon()`: the time of the next Full Moon in the current lunar cycle (UNIX timestamp).
- - `first_quarter()`: the time of the first quarter in the current lunar cycle (UNIX timestamp).
- - `next_first_quarter()`: the time of the next first quarter in the current lunar cycle (UNIX timestamp).
- - `last_quarter()`: the time of the last quarter in the current lunar cycle (UNIX timestamp).
- - `next_last_quarter()`: the time of the next last quarter in the current lunar cycle (UNIX timestamp).
+ - `get('illumination')`: the illuminated fraction of the Moon (0 = New, 1 = Full).
+ - `get('age')`: the age of the Moon, in days.
+ - `get('distance')`: the distance of the Moon from the centre of the Earth (kilometres).
+ - `get('diameter')`: the angular diameter subtended by the Moon as seen by an observer at the centre of the Earth (degrees).
+ - `get('sundistance')`: the distance to the Sun (kilometres).
+ - `get('sundiameter')`: the angular diameter subtended by the Sun as seen by an observer at the centre of the Earth (degrees).
+ - `get_phase('new_moon')`: the time of the last New Moon (UNIX timestamp).
+ - `get_phase('next_new_moon')`: the time of the next New Moon (UNIX timestamp).
+ - `get_phase('full_moon')`: the time of the Full Moon in the current lunar cycle (UNIX timestamp).
+ - `get_phase('next_full_moon')`: the time of the next Full Moon in the current lunar cycle (UNIX timestamp).
+ - `get_phase('first_quarter')`: the time of the first quarter in the current lunar cycle (UNIX timestamp).
+ - `get_phase('next_first_quarter')`: the time of the next first quarter in the current lunar cycle (UNIX timestamp).
+ - `get_phase('last_quarter')`: the time of the last quarter in the current lunar cycle (UNIX timestamp).
+ - `get_phase('next_last_quarter')`: the time of the next last quarter in the current lunar cycle (UNIX timestamp).
  - `phase_name()`: the [phase name](http://aa.usno.navy.mil/faq/docs/moon_phases.php).
 
 ### Example
 
 	// create an instance of the class, and use the current time
 	$moon = new Solaris\MoonPhase();
-	$age = round( $moon->age(), 1 );
+	$age = round(moon->get('age'), 1);
 	$stage = $moon->phase() < 0.5 ? 'waxing' : 'waning';
-	$distance = round( $moon->distance(), 2 );
-	$next = gmdate( 'G:i:s, j M Y', $moon->next_new_moon() );
+	$distance = round($moon->get('distance'), 2);
+	$next = gmdate('G:i:s, j M Y', $moon->get_phase('next_new_moon'));
 	echo "The moon is currently $age days old, and is therefore $stage. ";
 	echo "It is $distance km from the centre of the Earth. ";
 	echo "The next new moon is at $next.";
--- Solaris/MoonPhase.php
+++ Solaris/MoonPhase.php
@@ -1,41 +1,76 @@
 <?php
 /**
- * Moon phase calculation class
- * Adapted for PHP from Moontool for Windows (http://www.fourmilab.ch/moontoolw/)
- * by Samir Shah (http://rayofsolaris.net)
- * License: MIT
- **/
+*
+* Moon phase calculation class
+* Adapted for PHP from Moontool for Windows (http://www.fourmilab.ch/moontoolw/)
+* by Samir Shah (http://rayofsolaris.net)
+* License: MIT
+*
+*/
 namespace Solaris;
 
-class MoonPhase {
-	private $timestamp;
-	private $phase;
-	private $illum;
-	private $age;
-	private $dist;
-	private $angdia;
-	private $sundist;
-	private $sunangdia;
+/**
+* MoonPhase class
+*/
+class MoonPhase
+{
+	/** @var int timestamp */
+	protected $timestamp;
 
-	private $synmonth;
+	/** @var float phase */
+	protected $phase;
 
-	private $quarters = null;
+	/** @var float illumination */
+	protected $illumination;
 
-	function __construct( $pdate = null ) {
-		if( is_null( $pdate ) )
-			$pdate = time();
+	/** @var float age */
+	protected $age;
 
-		/*  Astronomical constants  */
-		$epoch = 2444238.5;			// 1980 January 0.0
+	/** @var float distance */
+	protected $distance;
 
-		/*  Constants defining the Sun's apparent orbit  */
+	/** @var float diameter */
+	protected $diameter;
+
+	/** @var float sundistance */
+	protected $sundistance;
+
+	/** @var float sundiameter */
+	protected $sundiameter;
+
+	/** @var float synmonth */
+	protected $synmonth;
+
+	/** @var array quarters */
+	protected $quarters = false;
+
+	/**
+	* Constructor
+	*
+	* @param \DateTime|null $date
+	*/
+	public function __construct($date = null)
+	{
+		$date = time();
+
+		if ((null !== $date) && $date instanceof \DateTime)
+		{
+			$date = $date->getTimestamp();
+		}
+
+		$this->timestamp = $date;
+
+		// Astronomical constants. 1980 January 0.0
+		$epoch = 2444238.5;
+
+		// Constants defining the Sun's apparent orbit
 		$elonge = 278.833540;		// Ecliptic longitude of the Sun at epoch 1980.0
 		$elongp = 282.596403;		// Ecliptic longitude of the Sun at perigee
 		$eccent = 0.016718;			// Eccentricity of Earth's orbit
 		$sunsmax = 1.495985e8;		// Semi-major axis of Earth's orbit, km
 		$sunangsiz = 0.533128;		// Sun's angular size, degrees, at semi-major axis distance
 
-		/*  Elements of the Moon's orbit, epoch 1980.0  */
+		// Elements of the Moon's orbit, epoch 1980.0
 		$mmlong = 64.975464;		// Moon's mean longitude at the epoch
 		$mmlongp = 349.383063;		// Mean longitude of the perigee at the epoch
 		$mlnode = 151.950429;		// Mean longitude of the node at the epoch
@@ -45,21 +80,14 @@ class MoonPhase {
 		$msmax = 384401;			// Semi-major axis of Moon's orbit in km
 		$mparallax = 0.9507;		// Parallax at distance a from Earth
 		$synmonth = 29.53058868;	// Synodic month (new Moon to new Moon)
+
 		$this->synmonth = $synmonth;
-		$lunatbase = 2423436.0;		// Base date for E. W. Brown's numbered series of lunations (1923 January 16)
 
-		/*  Properties of the Earth  */
-		// $earthrad = 6378.16;				// Radius of Earth in kilometres
-		// $PI = 3.14159265358979323846;	// Assume not near black hole
+		// date is coming in as a UNIX timstamp, so convert it to Julian
+		$date = $date / 86400 + 2440587.5;
 
-		$this->timestamp = $pdate;
-
-		// pdate is coming in as a UNIX timstamp, so convert it to Julian
-		$pdate =  $pdate / 86400 + 2440587.5;
-
-		/* Calculation of the Sun's position */
-
-		$Day = $pdate - $epoch;								// Date within epoch
+		// Calculation of the Sun's position
+		$Day = $date - $epoch;								// Date within epoch
 		$N = $this->fixangle((360 / 365.2422) * $Day);		// Mean anomaly of the Sun
 		$M = $this->fixangle($N + $elonge - $elongp);		// Convert from perigee co-ordinates to epoch 1980.0
 		$Ec = $this->kepler($M, $eccent);					// Solve equation of Kepler
@@ -67,11 +95,11 @@ class MoonPhase {
 		$Ec = 2 * rad2deg(atan($Ec));						// True anomaly
 		$Lambdasun = $this->fixangle($Ec + $elongp);		// Sun's geocentric ecliptic longitude
 
-		$F = ((1 + $eccent * cos(deg2rad($Ec))) / (1 - $eccent * $eccent));	// Orbital distance factor
+		$F = ((1 + $eccent * cos(deg2rad($Ec))) / (1 - $eccent * $eccent)); // Orbital distance factor
 		$SunDist = $sunsmax / $F;							// Distance to Sun in km
 		$SunAng = $F * $sunangsiz;							// Sun's angular size in degrees
 
-		/* Calculation of the Moon's position */
+		// Calculation of the Moon's position
 		$ml = $this->fixangle(13.1763966 * $Day + $mmlong);				// Moon's mean longitude
 		$MM = $this->fixangle($ml - 0.1114041 * $Day - $mmlongp);		// Moon's mean anomaly
 		$MN = $this->fixangle($mlnode - 0.0529539 * $Day);				// Moon's ascending node mean longitude
@@ -89,9 +117,9 @@ class MoonPhase {
 		$x = cos(deg2rad($lPP - $NP));									// X inclination coordinate
 
 		$Lambdamoon = rad2deg(atan2($y, $x)) + $NP;						// Ecliptic longitude
-		$BetaM = rad2deg(asin(sin(deg2rad($lPP - $NP)) * sin(deg2rad($minc))));		// Ecliptic latitude
+		$BetaM = rad2deg(asin(sin(deg2rad($lPP - $NP)) * sin(deg2rad($minc)))); // Ecliptic latitude
 
-		/* Calculation of the phase of the Moon */
+		// Calculation of the phase of the Moon
 		$MoonAge = $lPP - $Lambdasun;									// Age of the Moon in degrees
 		$MoonPhase = (1 - cos(deg2rad($MoonAge))) / 2;					// Phase of the Moon
 
@@ -102,57 +130,85 @@ class MoonPhase {
 		$MoonAng = $mangsiz / $MoonDFrac;								// Moon's angular diameter
 		// $MoonPar = $mparallax / $MoonDFrac;							// Moon's parallax
 
-		// store results
+		// Store results
 		$this->phase = $this->fixangle($MoonAge) / 360;					// Phase (0 to 1)
-		$this->illum = $MoonPhase;										// Illuminated fraction (0 to 1)
+		$this->illumination = $MoonPhase;								// Illuminated fraction (0 to 1)
 		$this->age = $synmonth * $this->phase;							// Age of moon (days)
-		$this->dist = $MoonDist;										// Distance (kilometres)
-		$this->angdia = $MoonAng;										// Angular diameter (degrees)
-		$this->sundist = $SunDist;										// Distance to Sun (kilometres)
-		$this->sunangdia = $SunAng;										// Sun's angular diameter (degrees)
+		$this->distance = $MoonDist;									// Distance (kilometres)
+		$this->diameter = $MoonAng;										// Angular diameter (degrees)
+		$this->sundistance = $SunDist;									// Distance to Sun (kilometres)
+		$this->sundiameter = $SunAng;									// Sun's angular diameter (degrees)
 	}
 
-	private function fixangle($a) {
-		return ( $a - 360 * floor($a / 360) );
+	/**
+	* Fix angle
+	*
+	* @param float $a
+	* @return float
+	*/
+	protected function fixangle(float $a): float
+	{
+		return ($a - 360 * floor($a / 360));
 	}
 
-	//  KEPLER  --   Solve the equation of Kepler.
-	private function kepler($m, $ecc) {
-		$epsilon = 0.000001;	// 1E-6
+	/**
+	* Kepler
+	*
+	* @param float $m
+	* @param float $ecc
+	* @return float
+	*/
+	protected function kepler(float $m, float $ecc): float
+	{
+		// 1E-6
+		$epsilon = 0.000001;
 		$e = $m = deg2rad($m);
-		do {
+
+		do
+		{
 			$delta = $e - $ecc * sin($e) - $m;
-			$e -= $delta / ( 1 - $ecc * cos($e) );
+			$e -= $delta / (1 - $ecc * cos($e));
 		}
-		while ( abs($delta) > $epsilon );
+		while (abs($delta) > $epsilon);
+
 		return $e;
 	}
 
-	/*  Calculates  time  of  the mean new Moon for a given
-		base date.  This argument K to this function is the
-		precomputed synodic month index, given by:
-            K = (year - 1900) * 12.3685
-        where year is expressed as a year and fractional year.
+	/**
+	* Calculates time  of the mean new Moon for a given base date.
+	*	 This argument K to this function is the precomputed synodic month index, given by:
+	*	 K = (year - 1900) * 12.3685
+	*	 where year is expressed as a year and fractional year.
+	*
+	* @param int   $date
+	* @param float $k
+	* @return float
 	*/
-	private function meanphase($sdate, $k){
+	protected function meanphase(int $date, float $k): float
+	{
 		// Time in Julian centuries from 1900 January 0.5
-		$t = ( $sdate - 2415020.0 ) / 36525;
-		$t2 = $t * $t;
-		$t3 = $t2 * $t;
+		$jt = ($date - 2415020.0) / 36525;
+		$t2 = $jt * $jt;
+		$t3 = $t2 * $jt;
 
 		$nt1 = 2415020.75933 + $this->synmonth * $k
-				+ 0.0001178 * $t2
-				- 0.000000155 * $t3
-				+ 0.00033 * sin( deg2rad( 166.56 + 132.87 * $t - 0.009173 * $t2 ) );
+			+ 0.0001178 * $t2
+			- 0.000000155 * $t3
+			+ 0.00033 * sin(deg2rad(166.56 + 132.87 * $jt - 0.009173 * $t2));
 
 		return $nt1;
 	}
 
-	/*  Given a K value used to determine the mean phase of
-		the new moon, and a phase selector (0.0, 0.25, 0.5,
-		0.75), obtain the true, corrected phase time.
+	/**
+	* Given a K value used to determine the mean phase of the new moon and a
+	*	 phase selector (0.0, 0.25, 0.5, 0.75), obtain the true, corrected phase time.
+	*
+	* @param float $k
+	* @param float $phase
+	* @return float|null
 	*/
-	private function truephase($k, $phase){
+	protected function truephase(float $k, float $phase): ?float
+	{
 		$apcor = false;
 
 		$k += $phase;				// Add phase to new moon time
@@ -160,179 +216,205 @@ class MoonPhase {
 		$t2 = $t * $t;				// Square for frequent use
 		$t3 = $t2 * $t;				// Cube for frequent use
 		$pt = 2415020.75933			// Mean time of phase
-			 + $this->synmonth * $k
-			 + 0.0001178 * $t2
-			 - 0.000000155 * $t3
-			 + 0.00033 * sin( deg2rad( 166.56 + 132.87 * $t - 0.009173 * $t2 ) );
+			+ $this->synmonth * $k
+			+ 0.0001178 * $t2
+			- 0.000000155 * $t3
+			+ 0.00033 * sin(deg2rad(166.56 + 132.87 * $t - 0.009173 * $t2));
 
 		$m = 359.2242 + 29.10535608 * $k - 0.0000333 * $t2 - 0.00000347 * $t3;			// Sun's mean anomaly
 		$mprime = 306.0253 + 385.81691806 * $k + 0.0107306 * $t2 + 0.00001236 * $t3;	// Moon's mean anomaly
 		$f = 21.2964 + 390.67050646 * $k - 0.0016528 * $t2 - 0.00000239 * $t3;			// Moon's argument of latitude
-		if ( $phase < 0.01 || abs( $phase - 0.5 ) < 0.01 ) {
-		   // Corrections for New and Full Moon
-			$pt +=  (0.1734 - 0.000393 * $t) * sin( deg2rad( $m ) )
-					+ 0.0021 * sin( deg2rad( 2 * $m ) )
-					- 0.4068 * sin( deg2rad( $mprime ) )
-					+ 0.0161 * sin( deg2rad( 2 * $mprime) )
-					- 0.0004 * sin( deg2rad( 3 * $mprime ) )
-					+ 0.0104 * sin( deg2rad( 2 * $f ) )
-					- 0.0051 * sin( deg2rad( $m + $mprime ) )
-					- 0.0074 * sin( deg2rad( $m - $mprime ) )
-					+ 0.0004 * sin( deg2rad( 2 * $f + $m ) )
-					- 0.0004 * sin( deg2rad( 2 * $f - $m ) )
-					- 0.0006 * sin( deg2rad( 2 * $f + $mprime ) )
-					+ 0.0010 * sin( deg2rad( 2 * $f - $mprime ) )
-					+ 0.0005 * sin( deg2rad( $m + 2 * $mprime ) );
-			$apcor = true;
-		} else if ( abs( $phase - 0.25 ) < 0.01 || abs( $phase - 0.75 ) < 0.01 ) {
-			$pt +=  (0.1721 - 0.0004 * $t) * sin( deg2rad( $m ) )
-					+ 0.0021 * sin( deg2rad( 2 * $m ) )
-					- 0.6280 * sin( deg2rad( $mprime ) )
-					+ 0.0089 * sin( deg2rad( 2 * $mprime) )
-					- 0.0004 * sin( deg2rad( 3 * $mprime ) )
-					+ 0.0079 * sin( deg2rad( 2 * $f ) )
-					- 0.0119 * sin( deg2rad( $m + $mprime ) )
-					- 0.0047 * sin( deg2rad ( $m - $mprime ) )
-					+ 0.0003 * sin( deg2rad( 2 * $f + $m ) )
-					- 0.0004 * sin( deg2rad( 2 * $f - $m ) )
-					- 0.0006 * sin( deg2rad( 2 * $f + $mprime ) )
-					+ 0.0021 * sin( deg2rad( 2 * $f - $mprime ) )
-					+ 0.0003 * sin( deg2rad( $m + 2 * $mprime ) )
-					+ 0.0004 * sin( deg2rad( $m - 2 * $mprime ) )
-					- 0.0003 * sin( deg2rad( 2 * $m + $mprime ) );
-		if ( $phase < 0.5 )		// First quarter correction
-			$pt += 0.0028 - 0.0004 * cos( deg2rad( $m ) ) + 0.0003 * cos( deg2rad( $mprime ) );
-		else	// Last quarter correction
-			$pt += -0.0028 + 0.0004 * cos( deg2rad( $m ) ) - 0.0003 * cos( deg2rad( $mprime ) );
+
+		if ($phase < 0.01 || abs($phase - 0.5) < 0.01)
+		{
+			// Corrections for New and Full Moon
+			$pt += (0.1734 - 0.000393 * $t) * sin(deg2rad($m))
+				+ 0.0021 * sin(deg2rad(2 * $m))
+				- 0.4068 * sin(deg2rad($mprime))
+				+ 0.0161 * sin(deg2rad(2 * $mprime))
+				- 0.0004 * sin(deg2rad(3 * $mprime))
+				+ 0.0104 * sin(deg2rad(2 * $f))
+				- 0.0051 * sin(deg2rad($m + $mprime))
+				- 0.0074 * sin(deg2rad($m - $mprime))
+				+ 0.0004 * sin(deg2rad(2 * $f + $m))
+				- 0.0004 * sin(deg2rad(2 * $f - $m))
+				- 0.0006 * sin(deg2rad(2 * $f + $mprime))
+				+ 0.0010 * sin(deg2rad(2 * $f - $mprime))
+				+ 0.0005 * sin(deg2rad($m + 2 * $mprime));
+
 			$apcor = true;
 		}
-		if (!$apcor)	// function was called with an invalid phase selector
-			return false;
+		else if (abs($phase - 0.25) < 0.01 || abs($phase - 0.75) < 0.01)
+		{
+			$pt += (0.1721 - 0.0004 * $t) * sin(deg2rad($m))
+				+ 0.0021 * sin(deg2rad(2 * $m))
+				- 0.6280 * sin(deg2rad($mprime))
+				+ 0.0089 * sin(deg2rad(2 * $mprime))
+				- 0.0004 * sin(deg2rad(3 * $mprime))
+				+ 0.0079 * sin(deg2rad(2 * $f))
+				- 0.0119 * sin(deg2rad($m + $mprime))
+				- 0.0047 * sin(deg2rad($m - $mprime))
+				+ 0.0003 * sin(deg2rad(2 * $f + $m))
+				- 0.0004 * sin(deg2rad(2 * $f - $m))
+				- 0.0006 * sin(deg2rad(2 * $f + $mprime))
+				+ 0.0021 * sin(deg2rad(2 * $f - $mprime))
+				+ 0.0003 * sin(deg2rad($m + 2 * $mprime))
+				+ 0.0004 * sin(deg2rad($m - 2 * $mprime))
+				- 0.0003 * sin(deg2rad(2 * $m + $mprime));
 
-		return $pt;
+			// First and last quarter corrections
+			if ($phase < 0.5)
+			{
+				$pt += 0.0028 - 0.0004 * cos(deg2rad($m)) + 0.0003 * cos(deg2rad($mprime));
+			}
+			else
+			{
+				$pt += -0.0028 + 0.0004 * cos(deg2rad($m)) - 0.0003 * cos(deg2rad($mprime));
+				$apcor = true;
+			}
+		}
+
+		return $apcor ? $pt : null;
 	}
 
-	/* 	Find time of phases of the moon which surround the current date.
-		Five phases are found, starting and
-		ending with the new moons which bound the  current lunation.
+	/**
+	* Find time of phases of the moon which surround the current date. Five phases are found, starting and
+	*	 ending with the new moons which bound the current lunation.
+	*
+	* @return void
 	*/
-	private function phasehunt() {
-		$sdate = $this->utctojulian( $this->timestamp );
+	protected function phasehunt(): void
+	{
+		$sdate = $this->utc_to_julian($this->timestamp);
 		$adate = $sdate - 45;
 		$ats = $this->timestamp - 86400 * 45;
-		$yy = (int) gmdate( 'Y', $ats );
-		$mm = (int) gmdate( 'n', $ats );
+		$yy = (int) gmdate('Y', $ats);
+		$mm = (int) gmdate('n', $ats);
 
-		$k1 = floor( ( $yy + ( ( $mm - 1 ) * ( 1 / 12 ) ) - 1900 ) * 12.3685 );
-		$adate = $nt1 = $this->meanphase( $adate, $k1 );
+		$k1 = floor(($yy + (($mm - 1) * (1 / 12)) - 1900) * 12.3685);
+		$adate = $nt1 = $this->meanphase($adate, $k1);
 
-		while (true) {
+		while (true)
+		{
 			$adate += $this->synmonth;
 			$k2 = $k1 + 1;
 			$nt2 = $this->meanphase( $adate, $k2 );
-			// if nt2 is close to sdate, then mean phase isn't good enough, we have to be more accurate
-			if( abs( $nt2 - $sdate ) < 0.75 )
-				$nt2 = $this->truephase( $k2, 0.0 );
-			if ( $nt1 <= $sdate && $nt2 > $sdate )
+
+			// If nt2 is close to sdate, then mean phase isn't good enough, we have to be more accurate
+			if (abs($nt2 - $sdate) < 0.75)
+			{
+				$nt2 = $this->truephase($k2, 0.0);
+			}
+
+			if ($nt1 <= $sdate && $nt2 > $sdate)
+			{
 				break;
+			}
+
 			$nt1 = $nt2;
 			$k1 = $k2;
 		}
 
-		// results in Julian dates
-		$data = array(
-			$this->truephase( $k1, 0.0 ),
-			$this->truephase( $k1, 0.25 ),
-			$this->truephase( $k1, 0.5 ),
-			$this->truephase( $k1, 0.75 ),
-			$this->truephase( $k2, 0.0 ),
-			$this->truephase( $k2, 0.25 ),
-			$this->truephase( $k2, 0.5 ),
-			$this->truephase( $k2, 0.75 )
-		);
+		// Results in Julian dates
+		$dates = [
+			$this->truephase($k1, 0.0),
+			$this->truephase($k1, 0.25),
+			$this->truephase($k1, 0.5),
+			$this->truephase($k1, 0.75),
+			$this->truephase($k2, 0.0),
+			$this->truephase($k2, 0.25),
+			$this->truephase($k2, 0.5),
+			$this->truephase($k2, 0.75)
+		];
 
-		$this->quarters = array();
-		foreach( $data as $v )
-			$this->quarters[] = ( $v - 2440587.5 ) * 86400;	// convert to UNIX time
+		$this->quarters = [];
+		foreach ($dates as $jdate)
+		{
+			// Convert to UNIX time
+			$this->quarters[] = ($jdate - 2440587.5) * 86400;
+		}
 	}
 
-	/*  Convert UNIX timestamp to astronomical Julian time (i.e. Julian date plus day fraction).  */
-	private function utctojulian( $ts ) {
-		return $ts / 86400 + 2440587.5;
+	/**
+	* UTC to Julian
+	*
+	* @param int $ts
+	* @return float
+	*/
+	protected function utc_to_julian(int $timestamp): float
+	{
+		return $timestamp / 86400 + 2440587.5;
 	}
 
-	private function get_phase( $n ) {
-		if( is_null( $this->quarters ) )
-			$this->phasehunt();
-
-		return $this->quarters[$n];
-	}
-
-	/* Public functions for accessing results */
-
-	function phase(){
+	/**
+	* Get moon phase
+	*
+	* @return float
+	*/
+	public function phase(): float
+	{
 		return $this->phase;
 	}
 
-	function illumination(){
-		return $this->illum;
+	/**
+	* Get moon properties
+	*
+	* @param string $property_name
+	* @return int|float|array|null
+	*/
+	public function get(string $property_name)
+	{
+		return $this->{$property_name} ?? null;
 	}
 
-	function age(){
-		return $this->age;
+	/**
+	* Get moon phase data
+	*
+	* @param string $name
+	* @return float
+	*/
+	public function get_phase(string $name): ?float
+	{
+		$phases = [
+			'new_moon',
+			'first_quarter',
+			'full_moon', 
+			'last_quarter',
+			'next_new_moon',
+			'next_first_quarter',
+			'next_full_moon',
+			'next_last_quarter',
+		];
+
+		if ($this->quarters === false)
+		{
+			$this->phasehunt();
+		}
+
+		return $this->quarters[array_flip($phases)[$name]] ?? null;
 	}
 
-	function distance(){
-		return $this->dist;
-	}
+	/**
+	* Get current phase name
+	*	 There are eight phases, evenly split.
+	*	 A "New Moon" occupies the 1/16th phases either side of phase = 0, and the rest follow from that.
+	*
+	* @return string
+	*/
+	public function phase_name(): string
+	{
+		$names = [
+			'New Moon',
+			'Waxing Crescent',
+			'First Quarter',
+			'Waxing Gibbous',
+			'Full Moon',
+			'Waning Gibbous',
+			'Third Quarter',
+			'Waning Crescent',
+			'New Moon',
+		];
 
-	function diameter(){
-		return $this->angdia;
-	}
-
-	function sundistance(){
-		return $this->sundist;
-	}
-
-	function sundiameter(){
-		return $this->sunangdia;
-	}
-
-	function new_moon(){
-		return $this->get_phase( 0 );
-	}
-
-	function first_quarter(){
-		return $this->get_phase( 1 );
-	}
-
-	function full_moon(){
-		return $this->get_phase( 2 );
-	}
-
-	function last_quarter(){
-		return $this->get_phase( 3 );
-	}
-
-	function next_new_moon(){
-		return $this->get_phase( 4 );
-	}
-
-	function next_first_quarter(){
-		return $this->get_phase( 5 );
-	}
-
-	function next_full_moon(){
-		return $this->get_phase( 6 );
-	}
-
-	function next_last_quarter(){
-		return $this->get_phase( 7 );
-	}
-
-	function phase_name() {
-		$names = array( 'New Moon', 'Waxing Crescent', 'First Quarter', 'Waxing Gibbous', 'Full Moon', 'Waning Gibbous', 'Third Quarter', 'Waning Crescent', 'New Moon' );
-		// There are eight phases, evenly split. A "New Moon" occupies the 1/16th phases either side of phase = 0, and the rest follow from that.
-		return $names[ floor( ( $this->phase + 0.0625 ) * 8 ) ];
+		return $names[floor(($this->phase + 0.0625) * 8)];
 	}
 }
